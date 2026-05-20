77769e6e800c863c2d0cc251684cfe77b9cbd896
muhmdramah <mohammedrammah83@gmail.com>
2026-05-20 18:10:28 +0300

    Add MechanicShopConstants with tax rate and system user
    Introduced MechanicShopConstants class in the Common.Constants namespace, defining TaxRate and SystemUser constants for consistent use across the application. Imported System.Globalization namespace.

---
+++ src/MechanicShop.Domain/Common/Constants/MechanicShopConstants.cs
@@ -0,0 +1,9 @@
+using System.Globalization;
+namespace MechanicShop.Domain.Common.Constants
+{
+    public class MechanicShopConstants
+    {
+        public const decimal TaxRate = 0.15m;
+        public const string SystemUser = "System";
+    }
+}
